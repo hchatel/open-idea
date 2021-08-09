b0edf75e900b59ebde08e6f4e78ca014aa22caec
Hugo <chatelhugo@hotmail.fr>
2021-08-09 13:20:02 +0200

    Initial app commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Nav from '../components/Nav.svelte';
+</script>
+
+<svelte:head>
+    <title>My test app</title>
+</svelte:head>
+
+<Nav />
+
+<slot />
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
