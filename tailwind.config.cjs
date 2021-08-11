62e8c55e8aa967fc64e648fc7f21b7e035009395
Hugo <chatelhugo@hotmail.fr>
2021-08-11 13:36:38 +0200

    Add: Idea card

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      gray: colors.warmGray,
+      green: colors.lime,
+    },
     extend: {},
   },
   variants: {
